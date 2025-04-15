97663cd24d7cc9c2e3b7ca9ecc2b8747870534d3
Zatherend <maxalvareznieto@gmail.com>
2025-04-15 10:21:59 -0400

    Prueba codigo

--- hola.py
+++ hola.py
@@ -1,1 +1,24 @@
-print ("Hola mundo")
+# variables 
+# nombre="Maxi"
+edad=20
+
+# nombre=input()
+
+
+#ejemplo de concatenacion
+# print ("Hola" , nombre, "y su edad es", edad)
+
+# print ("Hola" , nombre, "y su edad en 5 años será", edad+5)
+print (2
+    "ingrese 2 numeros")
+n1=int(input())
+n2=int(input())
+
+print (n1+n2)
+# n1=4
+# n2=6
+# print (n1+n2)
+
+# n1=4
+# n2=6
+# print (n1+n2)
